58051c27f95bba8c090249a393549f535105166e
burke86 <colin.burke.j@gmail.com>
2026-02-05 17:04:01 -0500

    fix flatten_util

--- src/eztaox/models.py
+++ src/eztaox/models.py
@@ -176,7 +176,7 @@ class MultiVarModel(eqx.Module):
         Returns:
             JAXArray: AIC value.
         """
-        k = jax.ravel_util.ravel_pytree(params).size
+        k = jax.flatten_util.ravel_pytree(params)[0].size
         log_likelihood = self.log_prob(params)
         return 2 * k - 2 * log_likelihood
 
@@ -189,7 +189,7 @@ class MultiVarModel(eqx.Module):
             JAXArray: BIC value.
         """
         n = self.y.size
-        k = jax.ravel_util.ravel_pytree(params).size
+        k = jax.flatten_util.ravel_pytree(params).size
         log_likelihood = self.log_prob(params)
         return jnp.log(n) * k - 2 * log_likelihood
 
